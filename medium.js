e9534d9228fe82941271aa22c7f6ecd26074d832
FinnZan <FinnZan@gmail.com>
2019-03-06 23:14:24 +0800

    238. Product of Array Except Self

--- medium.js
+++ medium.js
@@ -383,7 +383,7 @@ Medium = {
                 if (s[start + i] != s[end - i]) {
                     return false;
                 }
-            }            
+            }
             return true;
         }
 
@@ -398,5 +398,21 @@ Medium = {
         }
 
         return count;
+    },
+
+    // 238. Product of Array Except Self
+    productExceptSelf: function (nums) {
+        var n = nums.length;
+        res = [];
+        res[0] = 1;
+        for (var i = 1; i < n; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+        var right = 1;
+        for (var i = n - 1; i >= 0; i--) {
+            res[i] *= right;
+            right *= nums[i];
+        }
+        return res;
     }
 }
