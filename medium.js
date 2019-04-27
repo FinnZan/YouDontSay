5fa4fd30f01a9c9c6291d04afd51b7bedbed607a
FinnZan <FinnZan@gmail.com>
2019-04-27 21:22:57 +0800

    912. Sort an Array

--- medium.js
+++ medium.js
@@ -530,5 +530,12 @@ Medium = {
         }
 
         return ret;
+    },
+    // 912. Sort an Array
+    sortArray: function (nums) {
+        nums.sort(function (a, b) {
+            return a - b
+        });
+        return nums;
     }
 }
